80d1109baed5e2682d59b3b9f535f866004b2698
arthurdiluz <arthurdiluz@outlook.com>
2021-11-28 18:44:00 -0300

    feat: get instagram profile images

--- src/index.js
+++ src/index.js
@@ -1,11 +1,46 @@
 const puppeteer = require('puppeteer');
+const path = require('path');
+const fs = require('fs');
 
-(async () => {
-  const browser = await puppeteer.launch()
-  const page = await browser.newPage()
+(async (username = 'arthurdiluz') => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  const profile_url = `https://www.instagram.com/${username}/`;
   
-  await page.goto('https://arthurdiluz.github.io/')
-  await page.screenshot({ path: './src/screenshots/portfolio.png'})
+  await page.goto(profile_url);
 
-  await browser.close()
+  imagesList = await page.evaluate(() => {
+    // get a node list of all images
+    const imgsNodeList = document.querySelectorAll('article img');
+
+    // transform NodeList in an array (array of elements)
+    const imgsArray = [... imgsNodeList];
+
+    // returns the image URL of each element
+    return imgsArray.map(image => { return image['src']});
+  });
+  
+  // creates a JSON with all images URL
+  try {
+    // creates path to JSON
+    fs.mkdirSync(path.join('api', 'profiles', username), { recursive: true }, error => {
+      if (error) throw Error(error);
+      console.warn('path created');
+    })
+
+    // creates JSON with images URL
+    fs.writeFile(
+      path.resolve('api', 'profiles', username, 'images.json'),
+      JSON.stringify(imagesList, null, 2),
+      'utf-8',
+      error => {
+        if (error) throw Error(error);
+        console.log(`${imagesList.length} downloaded`);
+      })
+  } catch (error) {
+    console.error(error);
+    return error;
+  }
+  
+  await browser.close(); // closes browser
 })();
