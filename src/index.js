2bd1820d73fa4bc42c0f481af65480fe0b7ad548
arthurdiluz <arthurdiluz@outlook.com>
2021-11-28 23:48:59 -0300

    refact: download images

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const puppeteer = require('puppeteer');
+const axios = require('axios').default;
 const path = require('path');
 const fs = require('fs');
 
@@ -6,41 +7,37 @@ const fs = require('fs');
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   const profile_url = `https://www.instagram.com/${username}/`;
-  
-  await page.goto(profile_url);
 
-  imagesList = await page.evaluate(() => {
-    // get a node list of all images
-    const imgsNodeList = document.querySelectorAll('article img');
-
-    // transform NodeList in an array (array of elements)
-    const imgsArray = [... imgsNodeList];
-
-    // returns the image URL of each element
-    return imgsArray.map(image => { return image['src']});
-  });
-  
-  // creates a JSON with all images URL
   try {
-    // creates path to JSON
-    fs.mkdirSync(path.join('api', 'profiles', username), { recursive: true }, error => {
-      if (error) throw Error(error);
-      console.warn('path created');
-    })
+    await page.goto(profile_url);
+    
+    imagesURL = await page.evaluate(() => {
+      // get a node list of all images
+      const imgsNodeList = document.querySelectorAll('article img');
 
-    // creates JSON with images URL
-    fs.writeFile(
-      path.resolve('api', 'profiles', username, 'images.json'),
-      JSON.stringify(imagesList, null, 2),
-      'utf-8',
-      error => {
-        if (error) throw Error(error);
-        console.log(`${imagesList.length} downloaded`);
-      })
+      if (!imgsNodeList.length) throw Error('no picture found');
+      
+      // transform NodeList in an array (array of elements)
+      const imgsArray = [...imgsNodeList];
+      
+      // returns the image URL of each element
+      return imgsArray.map(image => { return image['src'] });
+    });
+    
+    imagesURL.forEach(url => {
+      // creates the directory and download the profile images
+      axios.get(url, { responseType: 'stream' }).then(response => {
+          const directory = path.join('download', 'profiles', username, 'feed')
+          const filename = `${username}-${Date.now()}`
+          
+          fs.mkdirSync(directory, { recursive: true })
+          response.data.pipe(fs.createWriteStream(`${directory}/${filename}.png`))
+        });
+    });
   } catch (error) {
     console.error(error);
     return error;
+  } finally {
+    await browser.close();
   }
-  
-  await browser.close(); // closes browser
 })();
